d4cd000faba220bf08415d499ece685b6b424f6a
Erik Perez <erik.p.suarez@gmail.com>
2024-10-11 18:55:50 -0600

    Creacion del proyecto con las depedencias necesarias

---
+++ src/main/java/com/application/SpringBootRestProjectManagement/SpringBootRestProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.application.SpringBootRestProjectManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestProjectManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/SpringBootRestProjectManagement/SpringBootRestProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.SpringBootRestProjectManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
